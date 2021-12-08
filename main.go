16d4e7a736a585d559d8b9eacab48df65b9ec8ad
bimalsharma1443 <bimal.developer1443@gmail.com>
2021-12-08 22:31:11 +0530

    something done

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func visits(c *gin.Context) {
 	// ctx := context.Background()
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis-server:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
